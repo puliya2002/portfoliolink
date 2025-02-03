98cbf61d32537deb33037a730ad0df2944ed93b9
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-02-03 12:26:48 +0530

    navbar mapping

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import NavbarLinks from "../constants/navbar";
 
 const Navbar = () => {
     return (
-      <div className="flex items-center justify-between container mx-auto bg-gray-200 rounded-full my-4 px-5 py-4">
+      <div className="flex items-center justify-between container mx-auto bg-white rounded-full my-4 px-5 py-4">
         <div>
           <Image src="/logo.png" alt="logo" width={150} height={100}></Image>
         </div>
 
         <div>
-          <ul className="flex gap-4 items-center">
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+          <ul className="flex gap-6 items-center text-lg">
+            {NavbarLinks.map((link) => (
+              <li key={link.id}>
+                <Link href={`/${link.id}`}>{link.title}</Link>
+              </li>
+            ))}
             <li className="bg-black text-white rounded-full px-7 py-2">Sign Up</li>
             
           </ul>
---
+++ constants/navbar.js
@@ -0,0 +1,22 @@
+const exp = require("constants");
+
+const navbarLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "works",
+        title: "Works",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+export default navbarLinks;
