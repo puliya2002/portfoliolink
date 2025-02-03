7df20ef008b9121f205578b3edc520b218da9137
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-02-03 15:01:30 +0530

    mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,27 +2,34 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavbarLinks from "../constants/navbar";
+import { SquareMenu } from "lucide-react";
 
 const Navbar = () => {
-    return (
+  return (
+    <div className="sticky top-0 px-5">
       <div className="flex items-center justify-between container mx-auto bg-white rounded-full my-4 px-5 py-4">
         <div>
-          <Image src="/logo.png" alt="logo" width={150} height={100}></Image>
+          <Link href="/">
+            <Image src="/logo.png" alt="logo" width={150} height={100}></Image>
+          </Link>
         </div>
 
         <div>
-          <ul className="flex gap-6 items-center text-lg">
+          <ul className="flex gap-6 items-center text-lg hidden md:flex">
             {NavbarLinks.map((link) => (
               <li key={link.id}>
                 <Link href={`/${link.id}`}>{link.title}</Link>
               </li>
             ))}
-            <li className="bg-black text-white rounded-full px-7 py-2">Sign Up</li>
-            
+            <li className="bg-black text-white rounded-full px-7 py-2">
+              Sign Up
+            </li>
           </ul>
+          <SquareMenu className="md:hidden cursor-pointer" size={30} />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
