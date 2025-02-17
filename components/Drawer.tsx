9331d5cf8edc6b5703150606faffa9e84989e52a
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-02-17 08:16:32 +0530

    session for login page, register page and navbar

---
+++ components/Drawer.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { SquareMenu } from "lucide-react";
+import Link from "next/link";
+import navbarLinks from "../constants/navbar";
+import { useSession } from "next-auth/react";
+
+export default function MobileDrawer() {
+
+  const { data: session, status } = useSession();
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <SquareMenu className="cursor-pointer" size={30} />
+      </SheetTrigger>
+      <SheetContent className="bg-white">
+        <SheetTitle>Menu</SheetTitle>
+
+        <div className="pt-12">
+          <ul className="flex flex-col gap-3 text-lg ml-1">
+            {navbarLinks.map((link) => (
+              <li key={link.id} className={`${session ? "last:hidden" : ""}`}>
+                <Link href={`/${link.id}`}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+          <Link href="/register">
+            <button className="bg-black text-white rounded-full px-7 py-2 hover:bg-[--primary] hover:text-black mt-5">
+              {!session ? "Sign Up" : "Dashboard"}
+            </button>
+          </Link>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
---
+++ constants/navbar.js
@@ -0,0 +1,30 @@
+const exp = require("constants");
+
+const navbarLinks = [
+  {
+    id: "/",
+    title: "Home",
+  },
+  {
+    id: "template",
+    title: "Template",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "works",
+    title: "Works",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "login",
+    title: "Login",
+  },
+];
+
+export default navbarLinks;
