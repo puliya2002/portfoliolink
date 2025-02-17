8306cc56fa35f27cfc243c0c8d76036292e06e97
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-02-17 09:03:06 +0530

    navbar session desktop

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import NavbarLinks from "../constants/navbar";
 import Drawer from "./Drawer";
+import { useSession } from "next-auth/react";
 
 
 const Navbar = () => {
+
+
+  const { data: session } = useSession();
 
   return (
     <div className="sticky top-0 px-5">
@@ -18,19 +22,26 @@ const Navbar = () => {
 
         <div>
           <ul className="flex gap-6 items-center text-md hidden md:flex ">
-            {NavbarLinks.map((link) => (
-              <li key={link.id} className="cursor-pointer hover:border-b hover:border-b-black">
-                <Link href={`/${link.id}`}>{link.title}</Link>
-              </li>
-            ))}
-            <Link href="/login">
-              <button className="bg-black text-white rounded-full px-7 py-2 hover:bg-[--primary] hover:text-black" >
-              Sign Up
-            </button>
+            <ul className="flex gap-6 items-center text-md hidden md:flex">
+              {NavbarLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`cursor-pointer hover:border-b hover:border-b-black${session ? " last:hidden" : ""}`}
+                >
+                  <Link href={`/${link.id}`}>{link.title}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link href="/register">
+              <button className="bg-black text-white rounded-full px-7 py-2 hover:bg-[--primary] hover:text-black">
+                {session ? "Dashboard" : "Sign Up"}
+              </button>
             </Link>
           </ul>
 
-          <div className="md:hidden pt-1"><Drawer /></div>
+          <div className="md:hidden pt-1">
+            <Drawer />
+          </div>
         </div>
       </div>
     </div>
