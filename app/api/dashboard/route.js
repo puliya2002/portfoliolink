85e0a1f1b440aeb930c5cec4196b0bc955f9c023
Pulindu Vidmal <vidmalpulindu@gmail.com>
2025-04-08 10:46:11 +0530

    fix test

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -1,7 +1,7 @@
 import connectDB from "@/lib/mongodb";
 import User from "@/lib/models/user";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 import { NextResponse } from "next/server";
 
 export async function GET() {
